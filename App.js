51b3b39c58201ee837899deb6d67044cc761f3e3
Abdelalim dev <abdelalim.dev@gmail.com>
2022-03-06 11:30:37 +0100

    Navigation skeleton for logged or guest users

--- App.js
+++ App.js
@@ -6,53 +6,23 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Home, Auth } from './src/screens'
+import { SafeAreaView } from 'react-native';
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>Hi there</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        {isLoggedIn ?
+          <Home /> :
+          <Auth />
+        }
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
