055d698924228e509d8ecdf4a5a85dfefceac78d
Abdelalim dev <abdelalim.dev@gmail.com>
2022-03-13 18:18:06 +0100

    Basic inputs for the register screen

--- src/i18n/locals/en.js
+++ src/i18n/locals/en.js
@@ -1,4 +1,12 @@
 const translation = {
-    lang: "English"
+    lang: "English",
+    phone1: "Phone 1",
+    sim1: "SIM 1",
+    phone2: "Phone 2",
+    sim2: "SIM 2",
+    bank: "Bank name",
+    iban: "Bank number",
+    required: "required",
+    optional: "optional",
 };
 export default translation;
--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,20 +1,68 @@
-import { View, Text, SafeAreaView, Button } from 'react-native'
-import React, { useContext } from 'react'
+import { Text, Button } from 'react-native'
+import React, { useContext, useState } from 'react'
 import { SessionContext } from '../../App'
-import i18next from 'i18next';
+import { Input, SafeArea } from '../components'
 
 export default function Auth() {
     const session = useContext(SessionContext)
-
+    const [phone, setPhone] = useState("");
+    const [phoneErrMsg, setPhoneErrMsg] = useState("")
+    const [phone2, setPhone2] = useState("");
+    const [phone2ErrMsg, setPhone2ErrMsg] = useState("")
+    const [iban, setIban] = useState("");
+    const [ibanErrMsg, setIbanErrMsg] = useState("")
+    const [bank, setBank] = useState("");
+    const [bankErrMsg, setBankErrMsg] = useState("")
 
     const login = () => {
         session.changeSession(true)
     }
 
     return (
-        <SafeAreaView>
+        <SafeArea>
             <Text>Language is {_('lang')}</Text>
             <Button title="Login" onPress={login} />
-        </SafeAreaView>
+
+            <Input
+                keyboardType="phone-pad"
+                autoComplete="tel"
+                label={_('phone1')}
+                value={phone}
+                onChangeText={text => setPhone(text)}
+                params={{
+                    icon: 'phone',
+                    errorMessage: phoneErrMsg,
+                }}
+            />
+            <Input
+                keyboardType="phone-pad"
+                label={_('phone2')}
+                value={phone2}
+                onChangeText={text => setPhone2(text)}
+                params={{
+                    icon: 'phone',
+                    errorMessage: phone2ErrMsg,
+                }}
+            />
+            <Input
+                label={_('iban')}
+                value={iban}
+                onChangeText={text => setIban(text)}
+                params={{
+                    icon: 'bank',
+                    errorMessage: ibanErrMsg,
+                }}
+            />
+            <Input
+                keyboardType="phone-pad"
+                label={_('bank')}
+                value={bank}
+                onChangeText={text => setBank(text)}
+                params={{
+                    icon: 'bank',
+                    errorMessage: bankErrMsg,
+                }}
+            />
+        </SafeArea>
     )
 }
