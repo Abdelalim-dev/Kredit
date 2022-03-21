1f6291a00cb756a19408c7b44c9d657275e81993
Abdelalim dev <abdelalim.dev@gmail.com>
2022-03-21 09:58:23 +0100

    Focus the next field in the form

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -20,6 +20,11 @@ const Image = styled.Image`
     align-self: center;
 `
 
+const FIELD_PHONE = "phone"
+const FIELD_IBAN = "iban"
+const FIELD_PHONE2 = "phone2"
+const FIELD_BANK = "bank"
+
 export default function Auth() {
     const session = useContext(SessionContext)
     const [phone, setPhone] = useState("");
@@ -46,6 +51,7 @@ export default function Auth() {
     const ibanRef = React.useRef()
     const phone2Ref = React.useRef()
     const op2Ref = React.useRef()
+    const bankRef = React.useRef()
 
     const register = () => {
         const isValid = validateForm()
@@ -102,6 +108,24 @@ export default function Auth() {
 
     const handleExpansion = () => setExpanded(!accordioExpanded)
 
+    const onReturn = (field) => {
+        switch (field) {
+            case FIELD_PHONE:
+                ibanRef.current.focus()
+                break
+            case FIELD_IBAN:
+                if (accordioExpanded) phone2Ref.current.focus()
+                else register()
+                break
+            case FIELD_PHONE2:
+                bankRef.current.focus()
+                break
+            case FIELD_BANK:
+                register()
+                break
+        }
+    }
+
     return (
         <SafeArea>
             <ScrollView>
@@ -115,6 +139,7 @@ export default function Auth() {
                     keyboardType="phone-pad"
                     returnKeyType="next"
                     autoComplete="tel"
+                    onSubmitEditing={() => onReturn(FIELD_PHONE)}
                     label={`${_('phone1')} (${_('phoneHint')})`}
                     value={phone}
                     onChangeText={text => setPhone(text)}
@@ -126,6 +151,7 @@ export default function Auth() {
                 <Input
                     ref={ibanRef}
                     returnKeyType={accordioExpanded ? "next" : "done"}
+                    onSubmitEditing={() => onReturn(FIELD_IBAN)}
                     label={_('iban')}
                     value={iban}
                     onChangeText={text => setIban(text)}
@@ -140,6 +166,7 @@ export default function Auth() {
                         ref={phone2Ref}
                         keyboardType="phone-pad"
                         returnKeyType="next"
+                        onSubmitEditing={() => onReturn(FIELD_PHONE2)}
                         label={`${_('phone2')} (${_('phoneHint')})`}
                         value={phone2}
                         onChangeText={text => setPhone2(text)}
@@ -149,7 +176,9 @@ export default function Auth() {
                     <OperatorMenu ref={op2Ref} label={_('sim2')} onItemSelected={setOp2} />
 
                     <Input
+                        ref={bankRef}
                         returnKeyType="done"
+                        onSubmitEditing={() => onReturn(FIELD_BANK)}
                         label={_('bank')}
                         value={bank}
                         onChangeText={text => setBank(text)}
