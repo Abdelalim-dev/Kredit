359813c4700b8a3065816ef28f62ff8bf2729fac
Abdelalim dev <abdelalim.dev@gmail.com>
2022-03-22 10:48:21 +0100

    Animated loader (Lottie)

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import LottieView from 'lottie-react-native';
 
 const LoaderContainer = styled.View`
     flex: 1;
     align-items: center;
     justify-content: center;
-    background-color: pink;
 `
-
-const Loading = styled.Image`
-`
-
 const Loader = () => (<LoaderContainer>
-    <Loading source={require('../assets/images/logo.png')} />
+    <LottieView source={require('src/assets/animations/loading.json')} autoPlay loop />
 </LoaderContainer>)
 
 
