33ee0da7a6cdc218cbbd899c0564fcf6dc379e75
Abdelalim dev <abdelalim.dev@gmail.com>
2022-03-23 00:01:50 +0100

    Disable/Enable button according to session data

--- src/screens/Balance.js
+++ src/screens/Balance.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Button } from '../components'
 import styled from 'styled-components'
-import { Paragraph, Headline } from 'react-native-paper'
+import { Caption as CaptionPaper, Headline, Title as TitlePaper } from 'react-native-paper'
 
 import bannerImage from '../assets/images/undraw_balance.png'
+import { SessionContext } from '../../App'
 
 const SafeArea = styled.SafeAreaView`
     flex:1;
@@ -29,13 +30,39 @@ const Row = styled.View`
     margin-top: 16px;
 `
 
-const BigButton = styled(Button)`
-    height: 150px;
-    width: 48%;
+const Column = styled.View`
+    align-items: center;
     justify-content: center;
 `
 
+const VSpace = styled.View`
+    width: 16px;
+`
+
+const Caption = styled(CaptionPaper)`
+    color: ${({ disabled }) => disabled ? '#989898' : '#FFF'};
+    text-align: center;
+`
+
+const Title = styled(TitlePaper)`
+    color: ${({ disabled }) => disabled ? '#989898' : '#FFF'};
+    text-align: center;
+`
+
+const BigButton = (props) => {
+    return (
+        <Button style={{ flex: 1, justifyContent: 'center' }}
+            contentStyle={{ height: 150 }} {...props}>
+            {props.children}
+        </Button>
+    )
+}
+
 export default function Balance() {
+    const SessionValue = React.useContext(SessionContext)
+    const { session: { phone, operator, phone2, operator2 } } = SessionValue
+    const disabled = !phone2
+
     return (
         <SafeArea>
             <Banner source={bannerImage} />
@@ -43,8 +70,21 @@ export default function Balance() {
             <Description>{_('screens.balance.title')}</Description>
 
             <Row>
-                <BigButton>Sim 1</BigButton>
-                <BigButton disabled>Sim 2</BigButton>
+                <BigButton>
+                    <Column>
+                        <Caption>SIM1</Caption>
+                        <Title>{operator}</Title>
+                    </Column>
+                </BigButton>
+
+                <VSpace />
+
+                <BigButton {...{ disabled }}>
+                    <Column>
+                        <Caption {...{ disabled }}>SIM2</Caption>
+                        {!!operator2 && <Title {...{ disabled }}>{operator2}</Title>}
+                    </Column>
+                </BigButton>
             </Row>
         </SafeArea>
     )
