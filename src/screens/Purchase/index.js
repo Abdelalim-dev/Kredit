bdf9ef1c72b3459952d2bdd16e1649dbccccaeaf
Abdelalim dev <abdelalim.dev@gmail.com>
2022-03-24 19:05:10 +0100

    Hid the header

--- src/screens/Purchase/index.js
+++ src/screens/Purchase/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import  PurchaseScreen  from './PurchaseScreen'
-import  Scan  from '../Scan'
-import {ROUTES} from '../../utils/Constants'
+import PurchaseScreen from './PurchaseScreen'
+import Scan from '../Scan'
+import { ROUTES } from '../../utils/Constants'
 
 
 const Stack = createNativeStackNavigator();
 
+const options = { headerShown: false }
+
 function PurchaseStack() {
-    return (
-        <Stack.Navigator>
-          <Stack.Screen name={ROUTES.PURCHASE} component={PurchaseScreen} />
-          <Stack.Screen name={ROUTES.SCAN} component={Scan} />
-        </Stack.Navigator>
-    );
-  }
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={ROUTES.PURCHASE} component={PurchaseScreen} options={options} />
+      <Stack.Screen name={ROUTES.SCAN} component={Scan} options={options} />
+    </Stack.Navigator>
+  );
+}
 
 
 export default PurchaseStack;
