b42d9577ffb4e40619b1c842ec9da871e6a9303a
Abdelalim <abdelalim.dev@gmail.com>
2023-08-05 01:14:59 +0100

    fix: function is imported directly now

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -185,7 +185,7 @@ export default function Settings(props) {
                     <SaveButton onPress={saveSettings}> {_('screens.settings.save')} </SaveButton>
 
                     {__DEV__ && <Button mode="outlined" onPress={() => {
-                        sessionValue.changeSession(null)
+                        changeSession(null)
                     }}>Logout</Button>}
                 </ScrollView>
             </KeyboardAvoidingView>
